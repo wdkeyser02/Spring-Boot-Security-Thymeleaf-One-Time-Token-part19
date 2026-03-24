50a3a79860829b389bb3122d09cfa2d25a4873a0
Willy De Keyser <spring.boot.programming@gmail.com>
2026-03-24 16:07:50 +0100

    Init

---
+++ Spring-Boot-Security-Thymeleaf-One-Time-Token-part19/src/main/java/willydekeyser/SpringBootSecurityThymeleafOneTimeTokenPart19Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafOneTimeTokenPart19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafOneTimeTokenPart19Application.class, args);
+	}
+
+}
